e7831273bccdd9885faef65a9c4e246a1f5cfc18
Vaishnav Venkat <vaishnavvenkat@Vaishnavs-MacBook-Pro.local>
2023-10-07 16:43:20 -0400

    changed requirements.txt and main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import cv2
 import numpy as np
+from PIL import Image
 
 st.title("Skin.AI")
 st.write("Vaishnav Venkat, Vignesh Venkat, Pranav Patil, Adarsh Narayan")
@@ -8,9 +9,12 @@ st.write("Vaishnav Venkat, Vignesh Venkat, Pranav Patil, Adarsh Narayan")
 label = "Upload an image of your skin or use camera to take a photo:"
 st.file_uploader(label)
 
-picture = st.camera_input("Use camera:")
+img_file_buffer = st.camera_input("Take a picture")
+if img_file_buffer is not None:
+    bytes_data = img_file_buffer.getvalue()
+    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
 
-if picture:
-    st.image(picture)
+    st.write(type(cv2_img))
 
+   # st.write(cv2_img.shape)
 
